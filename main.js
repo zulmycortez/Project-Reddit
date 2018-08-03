620e48e2422378cd11757547a24124dee29e0d72
zulmycortez <zulmycortez@gmail.com>
2018-08-03 11:51:23 -0400

    draft 1 is bare but progressing inshallah

---
+++ main.js
@@ -0,0 +1,24 @@
+// var button = document.getElementsByTagName('button')[0];
+//
+// button.addEventListener('click', function (){
+//   var userInputName = document.getElementById('name').value;
+//   var userInputMessage = document.getElementById('message'.value);
+//
+//   var li = document.createElement('li');
+//   var userInputElement = document.createTextNode(userInputName + '-' +userInputMessage);
+//
+//   li.appendChild(userInputElement);
+//
+//   document.getElementsByClassName('posts')[0].append(li);
+// });
+
+
+
+var addPosts = $('button').click(function(){
+  // var userName = $('#name').val();
+  // var userText = $('#text').val();
+
+  // $('#text').append("<li>" + $('#name').val() + "</li>");
+  alert("Handler for .click() class.");
+
+  });
