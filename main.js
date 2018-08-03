9bf9de4c19b82fe1d7759d098c56ee58edbbdf85
zulmycortez <zulmycortez@gmail.com>
2018-08-03 16:09:00 -0400

    we got some buttons! no functionality though....

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
-// var button = document.getElementsByTagName('button')[0];
-//
-// button.addEventListener('click', function (){
-//   var userInputName = document.getElementById('name').value;
-//   var userInputMessage = document.getElementById('message'.value);
-//
-//   var li = document.createElement('li');
-//   var userInputElement = document.createTextNode(userInputName + '-' +userInputMessage);
-//
-//   li.appendChild(userInputElement);
-//
-//   document.getElementsByClassName('posts')[0].append(li);
-// });
 
 
-
-$('button').click(function(){
-  // var userName = $('#name').val();
-  // var userText = $('#text').val();
-  $('.forum').append('<hr /><li>' +  $('#text').val() + '</li>');
-  $('.forum').append('<li><h5>' + 'Posted by: ' + '<b>'+ $('#name').val() + '</li></b></h4>');
-
-  // alert("Handler for .click() class.");
-
+$('button').click(function() {
+  var text = $('.forum').append('<hr><li>' + $('#text').val() + '</li>');
+  var name = $('.forum').append('<li><h5>' + 'Posted by: ' + '<b>' + $('#name').val() + '</li></b></h4>');
+  var comment = $('.forum').append('<a href="#comment"><span class="glyphicon glyphicon-comment" style="display:inline-block; width: 50px;"></span></a>');
+  var edit = $('.forum').append('<a href="#edit"><span class="glyphicon glyphicon-edit" style="display:inline-block; width: 50px;"></span></a>');
+  var remove =  $('.forum').append('<a href="#remove"><span class="glyphicon glyphicon-remove" style="display:inline-block; width: 50px;"></span></a>');
   });
