2b3314822b3e8925df6d3e472812e06bd4eccee2
zulmycortez <zulmycortez@gmail.com>
2018-08-05 18:57:58 -0400

    Remove button final

--- main.js
+++ main.js
@@ -1,9 +1,58 @@
-
+var comment;
+var edit;
+var remove;
+var postNum = 0;
 
 $('button').click(function() {
-  var text = $('.forum').append('<hr><li>' + $('#text').val() + '</li>');
-  var name = $('.forum').append('<li><h5>' + 'Posted by: ' + '<b>' + $('#name').val() + '</li></b></h4>');
-  var comment = $('.forum').append('<a href="#comment"><span class="glyphicon glyphicon-comment" style="display:inline-block; width: 50px;"></span></a>');
-  var edit = $('.forum').append('<a href="#edit"><span class="glyphicon glyphicon-edit" style="display:inline-block; width: 50px;"></span></a>');
-  var remove =  $('.forum').append('<a href="#remove"><span class="glyphicon glyphicon-remove" style="display:inline-block; width: 50px;"></span></a>');
+  var newPost = $('#forum').append('<div id="post'+ postNum +'"></div>');
+
+  var text = $('#post' + postNum).append('<hr><li>' + $('#text').val() + '</li>');
+  var name = $('#post' + postNum).append('<li><h5>' + 'Posted by: ' + '<b>' + $('#name').val() + '</li></b></h4>');
+  var comment = $('#post' + postNum).append('<a href="#comment"><span class="glyphicon glyphicon-comment" style="display:inline-block; width: 50px;"></span></a>');
+  var edit = $('#post' + postNum).append('<a href="#edit"><span class="glyphicon glyphicon-edit" style="display:inline-block; width: 50px;"></span></a>');
+  var remove =  $('#post' + postNum).append('<a class="remove glyphicon glyphicon-remove" style="display:inline-block; width: 50px;"></a>');
+
+  $('a.remove').click(function(e) {
+    $(e.target).parent().remove();
   });
+
+  postNum++;
+});
+
+
+
+// var li = document.createElemnet('li');
+//
+// var userInputElement = document.createTextNode(text + name + comment + edit + remove);
+//
+// li.appendChild(userInputElement);
+//
+// document.getElementByClassName('container')[0].append(li);
+
+// });
+
+
+// $('.container').append('<button id="comment" type="button"><a href="#comment"><span class="glyphicon glyphicon-comment" style="display:inline-block; width: 40px;"></span></a></button>'),
+// $('.container').append('<button id="edit" type="button"><a href="#edit"><span class="glyphicon glyphicon-edit" style="display:inline-block; width: 40px;"></span></a>'),
+// $('.container').append('<button id="remove" type="button"><a href="#remove"><span class="glyphicon glyphicon-remove" style="display:inline-block; width: 40px;"></span></a>'),
+
+
+// $('#remove').click(function(){
+//   this.remove();
+
+
+  //
+
+    // $("#dialog").dialog({ autoOpen: false });
+    // $('a').click(function () {
+    //     $("#dialog").dialog('open');
+
+
+
+// $('#comment').click(function(){
+//   $(this).data('clicked', true);
+// });
+//
+// if($('#comment').data('clicked')) {
+//   alert('yes!');
+// }
